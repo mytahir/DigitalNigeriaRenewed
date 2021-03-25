6c2ab18ee0a838a9a3ed9b3c45ee6770e8f22599
MUSTAPHA YUSUF <musty4000@gmail.com>
2021-03-25 03:02:22 -0700

    Digital Nigeria Renewed in Shell

---
+++ DigitalNigeriaRenewed/DigitalNigeriaRenewed/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DigitalNigeriaRenewed.Services;
+using DigitalNigeriaRenewed.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DigitalNigeriaRenewed
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DigitalNigeriaRenewed/DigitalNigeriaRenewed/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DigitalNigeriaRenewed.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DigitalNigeriaRenewed.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
